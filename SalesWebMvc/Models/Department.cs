a6c83af76deba52d02f19f160879fae911872bea
Sildinho Junior <sildojunior@gmail.com>
2021-03-27 19:10:51 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SalesWebMvc.Models
+﻿namespace SalesWebMvc.Models
 {
     public class Department
     {
